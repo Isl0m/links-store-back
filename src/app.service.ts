98bcdce99c6c34cb580adffb8ab0c7bf8b89afdf
Islom <90193909+Isl0m@users.noreply.github.com>
2022-09-25 16:55:29 +0500

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    return {
+      name: 'links-store-api',
+      version: '0.0.1',
+      description: 'An REST API with NESTJS and MONGODB',
+      author: 'Isl0m',
+    };
   }
 }
